62e76c555aca18fa9d4c3932f7aef372a33a09fb
Paul Trotter <paultrotter88@gmail.com>
2024-12-03 07:56:04 -0600

    feat: wip EditForm

--- BlazorWasmToDo/Pages/ToDo/ToDoFormModel.cs
+++ BlazorWasmToDo/Pages/ToDo/ToDoFormModel.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorWasmToDo.Pages.ToDo;
 
 public class ToDoFormModel
 {
     public string Text { get; set; } = string.Empty;
+    
     public DateTimeOffset? DueDate { get; set; }
 }
